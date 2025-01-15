66ee09afc4172b3e51774f7075f6006e6ee97d69
Alessandro Giorgetti <alessandro.giorgetti@live.com>
2025-01-15 16:00:10 +0100

    Added Failing Async Tests

---
+++ src/NEventStore.Persistence.MySql.Tests/PersistenceEngineFixtureAsync.cs
@@ -0,0 +1,45 @@
+﻿using NEventStore.Persistence.Sql.Tests;
+using NEventStore.Serialization.Binary;
+using global::MySql.Data.MySqlClient;
+using NEventStore.Persistence.Sql;
+using NEventStore.Persistence.Sql.SqlDialects;
+
+namespace NEventStore.Persistence.AcceptanceTests.Async
+{
+    public partial class PersistenceEngineFixtureAsync
+    {
+        public PersistenceEngineFixtureAsync()
+        {
+#if NET8_0_OR_GREATER
+            AppContext.SetSwitch("System.Runtime.Serialization.EnableUnsafeBinaryFormatterSerialization", true);
+#endif
+#if NET462
+            _createPersistence = pageSize =>
+            {
+                var serializer = new BinarySerializer();
+                return new SqlPersistenceFactory(
+                    new EnviromentConnectionFactory("MySql", "MySql.Data.MySqlClient"),
+                    serializer,
+                    new DefaultEventSerializer(serializer),
+                    new MySqlDialect(),
+                    pageSize: pageSize).Build();
+            };
+
+            // Wireup.Init().UsingSqlPersistence("test");
+#else
+            _createPersistence = pageSize =>
+            {
+                var serializer = new BinarySerializer();
+                return new SqlPersistenceFactory(
+                    new EnvironmentConnectionFactory("MySql", MySqlClientFactory.Instance),
+                    serializer,
+                    new DefaultEventSerializer(serializer),
+                    new MySqlDialect(),
+                    pageSize: pageSize).Build();
+            };
+
+            // Wireup.Init().UsingSqlPersistence("test");
+#endif
+        }
+    }
+}
